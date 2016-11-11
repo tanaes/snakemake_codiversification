669b417f6958028929f505828a61efcb90a10b6a
Jon Sanders <jsanders@oeb.harvard.edu>
2016-11-11 09:18:38 -0500

    added cluster execution files

---
+++ dag.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo "script.sh <out_dir (with config.yaml)> [additional snakemake arguments]*"
+    exit
+fi
+
+snakemake --directory "$@" --dag | dot -Tsvg > dag.svg
---
+++ launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo "script.sh <out_dir (with config.yaml)> [additional snakemake arguments]*"
+    exit
+fi
+
+snakemake -j 16 --local-cores 4 -w 90 --max-jobs-per-second 8 --cluster-config cluster.json --cluster "qsub -k eo -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}" --directory "$@"
